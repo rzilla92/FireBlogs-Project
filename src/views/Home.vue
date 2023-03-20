86475f1f1a0f2903205c23648347f5d8cf2051fc
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2023-03-20 13:08:02 +0700

    Blogs view & toggle edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -47,12 +47,11 @@ export default {
           blogCoverPhoto: "designed-for-everyone"
         }
       ],
-      sampleBlogCards : [
-        {blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May 1,2023"},
-        {blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May 1,2023"},
-        {blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May 1,2023"},
-        {blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "May 1,2023"},
-      ]
+    }
+  },
+  computed: {
+    sampleBlogCards() {
+      return this.$store.state.sampleBlogCards
     }
   }
 };
