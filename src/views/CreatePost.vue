e6178395528c68a7058e05cd095e4121519adb0d
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2023-03-31 11:09:49 +0700

    create post styling update

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -9,7 +9,7 @@
             <input type="text" placeholder="Enter Blog Title" v-model="blogTitle">
             <div class="upload-file">
                 <label for="blog-photo">Upload Cover Photo</label>
-                <input type="file" ref="blogPhoto" id="blog-photo" accept=".png, .jpg, /jpeg">
+                <input type="file" ref="blogPhoto" id="blog-photo" accept=".png, .jpg, ,jpeg" />
                 <button class="preview" :class="{ 'button-inactive': !this.$store.state.blogPhotoFileURL }">Preview Photo</button>
                 <span>File Chosen: {{ this.$store.state.blogPhotoName }}</span>
             </div>
@@ -48,7 +48,7 @@ export default {
             errorMsg: null,
             editorSettings: {
                 modules: {
-                    ImageResize: {}
+                    imageResize: {}
                 }
             }
         }
@@ -56,6 +56,155 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.create-post {
+  position: relative;
+  height: 100%;
 
+  button {
+    margin-top: 0;
+  }
+
+  .router-button {
+    text-decoration: none;
+    color: #fff;
+  }
+
+  label,
+  button,
+  .router-button {
+    transition: 0.5s ease-in-out all;
+    align-self: center;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius: 20px;
+    padding: 12px 24px;
+    color: #fff;
+    background-color: #303030;
+    text-decoration: none;
+
+    &:hover {
+      background-color: rgba(48, 48, 48, 0.7);
+    }
+  }
+
+  .container {
+    position: relative;
+    height: 100%;
+    padding: 10px 25px 60px;
+  }
+
+  // error styling
+  .invisible {
+    opacity: 0 !important;
+  }
+
+  .err-message {
+    width: 100%;
+    padding: 12px;
+    border-radius: 8px;
+    color: #fff;
+    margin-bottom: 10px;
+    background-color: #303030;
+    opacity: 1;
+    transition: 0.5s ease all;
+
+    p {
+      font-size: 14px;
+    }
+
+    span {
+      font-weight: 600;
+    }
+  }
+
+  .blog-info {
+    display: flex;
+    margin-bottom: 32px;
+    flex-direction: column;
+
+    @media (min-width: 900px) {
+        flex-direction: row;
+    }
+
+    input:nth-child(1) {
+      min-width: 300px;
+    }
+
+    input {
+      transition: 0.5s ease-in-out all;
+      padding: 10px 4px;
+      border: none;
+      border-bottom: 1px solid #303030;
+
+      &:focus {
+        outline: none;
+        box-shadow: 0 1px 0 0 #303030;
+      }
+    }
+
+    .upload-file {
+      flex: 1;
+      margin-left: 16px;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      margin-top: 5px;
+      gap: 5px;
+
+      @media (min-width: 900px) {
+        flex-direction: row;
+        margin-top: 0;
+        gap: 0;
+    }
+
+      input {
+        display: none;
+      }
+
+      .preview {
+        margin-left: 16px;
+        text-transform: initial;
+      }
+
+      span {
+        font-size: 12px;
+        margin-left: 16px;
+        align-self: center;
+      }
+    }
+  }
+
+  .editor {
+    height: 60vh;
+    display: flex;
+    flex-direction: column;
+
+    .quillWrapper {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+
+    .ql-container {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      overflow: scroll;
+    }
+
+    .ql-editor {
+      padding: 20px 16px 30px;
+    }
+  }
+
+  .blog-actions {
+    margin-top: 32px;
+
+    button {
+      margin-right: 16px;
+    }
+  }
+}
 </style>
